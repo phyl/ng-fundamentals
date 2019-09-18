fe27f55c5e62d86d78ac9411a05bce6d57c2e226
phyl <pmhindurwa@yahoo.com>
2019-09-18 14:10:56 +0200

    4

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,18 +6,30 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     template: `
     
 <div class="well hoverwell thumbnail">
-    <h2>{{event.name}}</h2>
-    <div>Date: {{event.date}}</div>
-    <div>Time: {{event.time}}</div>
-    <div>Price: R{{event.price}}</div>
-    <div>
-        <span>Location: {{event.location.address}}</span>
-        <span>{{event.location.city}}</span>
-        <span>{{event.location.country}}</span>
+    <h2>{{event?.name}}</h2>
+    <div>Date: {{event?.date}}</div>
+    <div></div>
+    <div [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+    </div>
+    <div>Price: R{{event?.price}}</div>
+    <div *ngIf="event?.location">
+        <span>Location: {{event?.location.address}}</span>
+        <span>{{event?.location.city}}</span>
+        <span>{{event?.location.country}}</span>
+    </div>
+    <div *ngIf="event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
     </div>
 
 </div>
-`
+`,
+styles: [`
+.thumbnail { min-height: 210px;}
+`]
 })
 
 export class EventThumbnailComponent {

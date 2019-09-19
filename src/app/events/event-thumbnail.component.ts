c9bfa972589f6d40f6308326bd14c31d6e68841f
phyl <pmhindurwa@yahoo.com>
2019-09-19 16:40:29 +0200

    Routing

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,7 +5,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     template: `
     
-<div class="well hoverwell thumbnail">
+<div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
     <div></div>
